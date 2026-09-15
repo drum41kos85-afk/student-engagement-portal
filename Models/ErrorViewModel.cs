58da24708dcfac7e4629070716dae0eded852580
Vladimir <drum41kos85@gmail.com>
2026-09-15 19:23:17 +0100

    Add remaining project files (csproj, wwwroot, default template files)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudentEngagementPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
